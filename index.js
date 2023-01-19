f60666c41aaaecf96eb850d90893f9a6e38901ca
Inga <inga.gudaite@gmail.com>
2023-01-19 13:32:13 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Serveris...');
+const app = {};
+
+//pradedam projekta
+app.init = () => {
+    //reikia ne visiems:
+    //trukstamo pradinio turinio generavimas:
+    // -- folder
+    // -- file
+
+    //gauti prisijungima prie DB
+    //uzkurti serveri (DBconnection)
+    // reguliarus procesai
+    // - info ssinchronizavimas/atsinaujinimas
+    // - failu archyvavimas
+    // - info agregavimas (statistika)
+    // - nereikalingos info salinimas:
+    //      - failu trinimas
+    //      - DB optimizavimas
+
+    console.log('pradedam...');
+}
+
+app.init();
+
+export { app };
